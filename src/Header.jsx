90b1a8a44b971a4538976761f9d42047eead30e6
nitikrimjial <nitiksinghrimjial@gmail.com>
2025-04-09 10:55:43 +0530

    List

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+
+
+function Header(){
+    return (
+      <>
+      <header>
+      <img src={reactLogo} className="logo" alt ="React Logo" />
+      </header>
+      </>
+    )
+  }
+  export default Header
