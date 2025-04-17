1ef384119d97d8bad034ec2608d19506b597e328
nitikrimjial <nitiksinghrimjial@gmail.com>
2025-04-17 08:19:45 +0530

    second

---
+++ src/footer.jsx
@@ -0,0 +1,12 @@
+function footer (){
+    return (
+        <footer>
+            <>
+            <h1> hi guys</h1>
+            </>
+
+        </footer>
+    )
+}
+
+export default footer
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import Footer from './footer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
     <App />
+    <Footer/>
   </StrictMode>,
 )
